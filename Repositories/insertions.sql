29dda878c1f597e722db1489ae4b180f455d7b44
RodrigoJesusSantistebanPachari <58257135+RodrigoJesusSantistebanPachari@users.noreply.github.com>
2021-08-11 02:12:26 -0500

    Add files via upload

---
+++ Repositories/insertions.sql
@@ -0,0 +1,4 @@
+INSERT INTO evento(nombre, descripcion, pais, cantidad_asistentes) VALUES
+("LibroShop", "Es un evento bueno", "Peru", "45"),
+("PorotosWeek", "Evento regular", "Chile", "47"),
+("ComputerWeek", "Evento de computacion", "Peru", "58");
