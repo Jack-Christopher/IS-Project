ff0f16f8cc116c113ad9fecb8453a3b7e1f6d369
Maycol <42551238+Sorbete-Sempai123@users.noreply.github.com>
2021-08-11 02:24:10 -0500

    Update Sesion.php

--- Domain/Models/Sesion.php
+++ Domain/Models/Sesion.php
@@ -1,8 +1,14 @@
 <?php
 
-class Sesion
+interface funcionalidad_get
 {
-    public $id;
+    public function get_values();
+    
+}
+
+class Sesion implements funcionalidad_get
+{
+    public  $id;
     public  $hora;
     public  $fecha;
     public  $InformacionExtra;
@@ -26,6 +32,56 @@ class Sesion
             "informacionExtra" => "$this->Evento",
         ];
         return $values;
+    }
+
+    public function setId($id)
+    {
+        $this->$id = $id;
+    }
+
+    public function getId()
+    {
+        return $this->$id;
+    }
+
+    public function setHora($hora)
+    {
+        $this->$hora = $hora;
+    }
+
+    public function getHora()
+    {
+        return $this->$hora;
+    }
+
+    public function setFecha($fecha)
+    {
+        $this->$fecha = $fecha;
+    }
+
+    public function getFecha()
+    {
+        return $this->$fecha;
+    }
+
+    public function setInformacionExtra($InformacionExtra)
+    {
+        $this->$InformacionExtra = $InformacionExtra;
+    }
+
+    public function getInformacionExtra()
+    {
+        return $this->$InformacionExtra;
+    }
+
+    public function setEvento($Evento)
+    {
+        $this->$Evento = $Evento;
+    }
+
+    public function getEvento()
+    {
+        return $this->$Evento;
     }
 
 }
